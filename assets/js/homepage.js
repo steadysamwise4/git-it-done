119347e63c91ae2c0f359615bb6c1653f953fada
Sam Davenport <samueldavenport@att.net>
2021-10-23 05:58:06 -0500

    added submit form to html, connected it with fetch api functionality

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,19 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+    event.preventDefault();
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -12,4 +28,4 @@ var getUserRepos = function(user) {
 
 };
 
-getUserRepos("steadysamwise4");
+userFormEl.addEventListener("submit", formSubmitHandler);
